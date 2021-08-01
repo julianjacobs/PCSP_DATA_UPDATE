15f36c73d012c1ebd037c2ddfc1e697db267d6d7
Julian <juja@umich.edu>
2021-08-01 16:35:49 -0400

    arguement input

--- PSCP_DATA_UPDATE/PSCP_DATA_UPDATE/main.cpp
+++ PSCP_DATA_UPDATE/PSCP_DATA_UPDATE/main.cpp
@@ -9,6 +9,19 @@ using namespace std;
 
 
 int main(int argc, char* argv[]) {
+	if (argc != 2) {
+		cout << "wrong input format";
+		return 0;
+	}
+	string fileName = argv[2];
+	fstream file;
+	file.open(fileName);
+	if (!file) {
+		cout << "bad file name";
+		return 0;
+	}
+
+
 
 
 }
