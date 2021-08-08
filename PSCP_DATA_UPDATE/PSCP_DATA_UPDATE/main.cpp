26213f7d97a6013a7ff5e6ddd688ce58ea94faa9
Julian <juja@umich.edu>
2021-08-07 21:32:07 -0400

    read csv file now

--- PSCP_DATA_UPDATE/PSCP_DATA_UPDATE/main.cpp
+++ PSCP_DATA_UPDATE/PSCP_DATA_UPDATE/main.cpp
@@ -1,5 +1,7 @@
 #include <fstream>
+#include <sstream>
 #include <iostream>
+#include <string>
 using namespace std;
 
 
@@ -9,19 +11,41 @@ using namespace std;
 
 
 int main(int argc, char* argv[]) {
+	//check if we have right amount of cmd arguements
 	if (argc != 2) {
 		cout << "wrong input format";
 		return 0;
 	}
-	string fileName = argv[2];
-	fstream file;
+	
+	//get file name
+	string fileName = argv[1];
+	
+	//initialize in and out streams
+	ofstream outFile("PI.txt");
+	ifstream file;
 	file.open(fileName);
-	if (!file) {
+	
+	//check if file got opened properly.
+	if (!file.is_open()) {
 		cout << "bad file name";
 		return 0;
 	}
+	
+	string currentLine;
+	//skip first line (excel header)
+	getline(file, currentLine);
+	//read other rows
+	while (getline(file, currentLine)) {
+		//read col
+		stringstream ss(currentLine);
+		string sku, productName;
+		//first value is sku
+		getline(ss, sku, ',');
+		//second value is name of sku which we want to edit
+		getline(ss, productName, ',');
 
-
+	}
+	
 
 
 }
